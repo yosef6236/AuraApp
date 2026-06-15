688ed12b336a538b83082ccb4c713e7a15b3e9ce
yosef6236 <yosef6236@gmail.com>
2026-06-15 22:26:29 +0300

    Fix Module.swift to be an Expo Module and restore AuraWidgetBundle

---
+++ widget/AuraWidgetBundle.swift
@@ -0,0 +1,9 @@
+import WidgetKit
+import SwiftUI
+
+@main
+struct AuraWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        AuraWidgetLiveActivity()
+    }
+}
--- widget/Module.swift
+++ widget/Module.swift
@@ -1,9 +1,72 @@
-import WidgetKit
-import SwiftUI
+import ExpoModulesCore
+import ActivityKit
 
-@main
-struct AuraWidgetBundle: WidgetBundle {
-    var body: some Widget {
-        AuraWidgetLiveActivity()
+public class ReactNativeWidgetExtensionModule: Module {
+    public func definition() -> ModuleDefinition {
+        Name("ReactNativeWidgetExtension")
+        
+        Function("areActivitiesEnabled") { () -> Bool in
+            let logger = Logger(logHandlers: [])
+            logger.info("areActivitiesEnabled()")
+            
+            if #available(iOS 16.2, *) {
+                return ActivityAuthorizationInfo().areActivitiesEnabled
+            } else {
+                return false
+            }
+        }
+        
+        Function("startActivity") { (remainingTime: Int) -> Void in
+            let logger = Logger(logHandlers: [])
+            logger.info("startActivity()")
+            
+            if #available(iOS 16.2, *) {
+                let attributes = AuraAppAttributes()
+                let contentState = AuraAppAttributes.ContentState(remainingTime: remainingTime)
+                
+                let activityContent = ActivityContent(state: contentState, staleDate: nil)
+                
+                do {
+                    let activity = try Activity.request(attributes: attributes, content: activityContent)
+                    logger.info("Requested a Live Activity \(String(describing: activity.id)).")
+                } catch (let error) {
+                    logger.info("Error requesting Live Activity \(error.localizedDescription).")
+                }
+            }
+        }
+
+        Function("updateActivity") { (remainingTime: Int) -> Void in
+            let logger = Logger(logHandlers: [])
+            logger.info("updateActivity()")
+            
+            if #available(iOS 16.2, *) {
+                let contentState = AuraAppAttributes.ContentState(remainingTime: remainingTime)
+                let updatedContent = ActivityContent(state: contentState, staleDate: nil)
+                
+                Task {
+                    for activity in Activity<AuraAppAttributes>.activities {
+                        await activity.update(updatedContent)
+                        logger.info("Updated the Live Activity: \(activity.id)")
+                    }
+                }
+            }
+        }
+        
+        Function("endActivity") { () -> Void in
+            let logger = Logger(logHandlers: [])
+            logger.info("endActivity()")
+            
+            if #available(iOS 16.2, *) {
+                let contentState = AuraAppAttributes.ContentState(remainingTime: 0)
+                let finalContent = ActivityContent(state: contentState, staleDate: nil)
+                
+                Task {
+                    for activity in Activity<AuraAppAttributes>.activities {
+                        await activity.end(finalContent, dismissalPolicy: .default)
+                        logger.info("Ending the Live Activity: \(activity.id)")
+                    }
+                }
+            }
+        }
     }
 }
